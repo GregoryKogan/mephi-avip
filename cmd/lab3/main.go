6231d4ad90273d2035be60014ff682a2aabeca72
Gregory Koganovsky <grisha.koganovskiy@gmail.com>
2025-03-01 15:28:12 +0300

    feat: use results of lab2 as input to lab3

--- cmd/lab3/main.go
+++ cmd/lab3/main.go
@@ -1,26 +1,31 @@
 package main
 
 import (
+	"os"
+
 	"github.com/GregoryKogan/mephi-av-processing/pkg/imgproc"
 )
 
 func main() {
-	img, _ := imgproc.OpenPNG("assets/salt-pepper-noise.png")
+	entries, _ := os.ReadDir("assets/noise/")
+	for _, file := range entries {
+		img, _ := imgproc.OpenPNG("assets/noise/" + file.Name())
 
-	halfTone := imgproc.GetLightness(img)
-	imgproc.SavePNG(halfTone, "output/lab3/original.png")
+		halfTone := imgproc.GetHalfTone(img)
+		imgproc.SavePNG(halfTone, "output/lab3/original-"+file.Name())
 
-	// 1. отфильтрованное монохромное (полутоновое) изображение;
-	filtered := imgproc.MedianFilter3x3(halfTone,
-		[3][3]int{
-			{1, 0, 1},
-			{0, 1, 0},
-			{1, 0, 1},
-		}, 3)
+		// 1. отфильтрованное монохромное (полутоновое) изображение;
+		filtered := imgproc.MedianFilter3x3(halfTone,
+			[3][3]int{
+				{1, 0, 1},
+				{0, 1, 0},
+				{1, 0, 1},
+			}, 3)
 
-	imgproc.SavePNG(filtered, "output/lab3/filtered.png")
+		imgproc.SavePNG(filtered, "output/lab3/filtered-"+file.Name())
 
-	// 2. разностное изображение (монохромный xor или модуль разности для полутона).
-	diff := imgproc.GetDifference(halfTone, filtered)
-	imgproc.SavePNG(diff, "output/lab3/difference.png")
+		// 2. разностное изображение (монохромный xor или модуль разности для полутона).
+		diff := imgproc.GetDifference(halfTone, filtered)
+		imgproc.SavePNG(diff, "output/lab3/difference-"+file.Name())
+	}
 }
